a5dda5ced3e95c9e3440b934e2a80aae3e0533cf
Gerben <info@gerbenvaneerten.nl>
2023-12-02 23:00:34 +0100

    Renamed to SHT3X

---
+++ src/SHT3X.h
@@ -0,0 +1,37 @@
+
+#ifndef __SHT3X_H
+#define __SHT3X_H
+
+#include "CRCSlow.h"
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include "Wire.h"
+
+class SHT3X
+{
+public:
+	SHT3X(uint8_t address = 0x45);
+    void begin();
+	uint8_t read();
+    float temperature();
+    float fahrenheit();
+    float kelvin();
+    float humidity();
+
+private:
+	uint8_t address;
+	uint16_t temperatureReading;
+	uint16_t humidityReading;
+    ootb::CRCSlow crcMethod = ootb::CRCSlow(0x31, 0xFF, 0x00);
+
+    bool checkCRC(uint8_t const *data, uint16_t length, uint8_t crc);
+};
+
+
+#endif
+
